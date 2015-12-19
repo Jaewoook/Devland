b9844fc9b974ec518ee476ab2675de1acd87c3ef
likepain <ajw4586@naver.com>
2015-12-19 21:03:35 +0900

    add list

---
+++ app/src/main/java/kr/melted/devand/adapter/PeopleAdapter.java
@@ -0,0 +1,54 @@
+package kr.melted.devand.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import kr.melted.devand.R;
+import kr.melted.devand.network.model.People;
+
+/**
+ * Created by Envy on 2015-12-19.
+ */
+public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.PeopleHolder> {
+
+
+    private static final int ITEMVIEW_LAYOUT_RESOURCE = R.layout.item_people_list;
+
+    private ArrayList<People> people;
+
+    public PeopleAdapter() {
+        people = new ArrayList<People>();
+    }
+
+    @Override
+    public PeopleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(ITEMVIEW_LAYOUT_RESOURCE, parent, false);
+        return new PeopleHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(PeopleHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class PeopleHolder extends RecyclerView.ViewHolder {
+
+
+        public TextView name, position, comment;
+
+        public PeopleHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/kr/melted/devand/network/model/People.java
@@ -0,0 +1,12 @@
+package kr.melted.devand.network.model;
+
+/**
+ * Created by Envy on 2015-12-19.
+ */
+public class People {
+
+    public String name;
+    public String _id;
+    public String position;
+    public String event_name;
+}
